19a115ef2524619dea6ece80fe3b6b8cf98cf07c
haddadanas <103462379+haddadanas@users.noreply.github.com>
2023-07-06 16:00:35 +0200

    Refactoring

--- hbt/ml/plotting/test_metrices.py
+++ hbt/ml/plotting/test_metrices.py
@@ -2,25 +2,37 @@ import unittest
 import numpy as np
 from metrices import *
 from tensorflow.math import confusion_matrix
+from sklearn.metrics import roc_curve
+
 
 class Test_Conf_Matrix(unittest.TestCase):
-    
+
+    @classmethod
+    def setUpClass(cls):
+        cls.test = np.random.randint(0,100, size=(1000,5))
+        cls.trues = np.random.randint(0,5,size= 1000)
+        cls.pred = np.divide(test.T, np.sum(test, axis=1)).T
+        cls.weights = np.random.randint(0,5, size = 1000)
+
+    def test_unvalid_inputs(self):
+        true_labels = np.array([1,0,1,0])
+        output = np.random
+        with self.assertRaises(ValueError):
+            get_conf_matrix(np.array([1,0,1,1,0]), self.pred)
+        with self.assertRaises(ValueError):
+            get_conf_matrix(self.trues, self.pred[:100])
+
+    def test_unvalid_weights(self):
+        pass
+
     def test_equals_no_weights(self):
-        test = np.random.randint(0,100, size=(1000,5))
-        trues = np.random.randint(0,5,size= 1000)
-        pred = np.divide(test.T, np.sum(test, axis=1)).T
-        weights = np.random.randint(0,5, size = 1000)
-        tf_matrix = confusion_matrix(trues, np.argmax(pred, axis = 1))
-        my_matrix = get_conf_matrix(trues,pred)
+        tf_matrix = confusion_matrix(self.trues, np.argmax(self.pred, axis = 1))
+        my_matrix = get_conf_matrix(self.trues,self.pred)
         self.assertTrue((tf_matrix.numpy() == my_matrix).all())
 
     def test_equals_with_weights(self):
-        test = np.random.randint(0,100, size=(1000,5))
-        trues = np.random.randint(0,5,size= 1000)
-        pred = np.divide(test.T, np.sum(test, axis=1)).T
-        weights = np.random.randint(0,5, size = 1000)
-        tf_matrix = confusion_matrix(trues, np.argmax(pred, axis = 1),weights=weights)
-        my_matrix = get_conf_matrix(trues,pred, weights=weights)
+        tf_matrix = confusion_matrix(self.trues, np.argmax(self.pred, axis = 1),weights=self.weights)
+        my_matrix = get_conf_matrix(self.trues,self.pred, weights=self.weights)
         self.assertTrue((tf_matrix.numpy() == my_matrix).all())
 
 if __name__ == '__main__':
