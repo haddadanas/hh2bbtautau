4ed84659040156f8c30e8c7eff4e10fa15ee6437
Anas Haddad <hhhaaanas@gmail.com>
2023-09-15 16:49:22 +0200

    output and require

--- hbt/ml/ml_plotting.py
+++ hbt/ml/ml_plotting.py
@@ -14,7 +14,11 @@ import luigi
 from columnflow.tasks.framework.base import Requirements, ShiftTask
 from columnflow.tasks.framework.plotting import PlotBase
 from columnflow.tasks.framework.decorators import view_output_plots
-from columnflow.util import DotDict
+from columnflow.util import DotDict, maybe_import
+
+
+np = maybe_import("numpy")
+ak = maybe_import("awkward")
 
 
 class MLPLottingChoices(Enum):
@@ -29,3 +33,33 @@ class PlotMLMetric(PlotBase):
         significant=True,
         description="Metric plotting function to use. Available choices: 'ConfusionMatrix', 'ROC'",
     )
+
+    def requires(self):
+        reqs = {
+            "events": self.reqs.MergeReducedEvents.req_different_branching(
+                self,
+                tree_index=self.branch,
+                branch=-1,
+            ),
+        }
+
+        return reqs
+
+    def output(self):
+        return {"plot": self.target(f"{self.plot_function}_{self.branch}.pdf"),
+                "array": self.target(f"array_{self.plot_function}_{self.branch}.parquet")}
+
+    @law.decorator.log
+    @law.decorator.localize
+    @law.decorator.safe_output
+    def run(self):
+        # prepare inputs and outputs
+        inputs = self.input()
+        output = self.output()
+        events = inputs['events']
+
+        test_if_int = np.vectorize(float.is_integer)
+        labels_mask = {cl_name: test_if_int(events.test[cl_name]).all() for cl_name in events.test.fields}
+        assert (sum(labels_mask.values()) == 1), (
+            f'Only one `true_labels` column per Network is expected, however {sum(labels_mask.values())} were found!'
+        )
