3252ee068b8ad1308663e2d65d5f6ac8d7494394
Anas Haddad <hhhaaanas@gmail.com>
2023-09-21 17:46:31 +0200

    Working events preperation

--- hbt/tasks/ml_plotting.py
+++ hbt/tasks/ml_plotting.py
@@ -23,7 +23,7 @@ np = maybe_import("numpy")
 ak = maybe_import("awkward")
 
 
-@inherits(MLEvaluationWrapper, MLEvaluation)
+@inherits(MLEvaluationWrapper) #, MLEvaluation)
 class PlotMLMetric(PlotBase):
 
     class MLPLottingChoices(Enum):
@@ -52,6 +52,35 @@ class PlotMLMetric(PlotBase):
         return {"plot": self.target(f"{self.plot_function}_{self.branch}.pdf"),
                 "array": self.target(f"array_{self.plot_function}_{self.branch}.parquet")}
 
+    # Should be overwritten when a proper get_target_labels function is defined
+    def get_target_labels(self, array, ind) -> np.ndarray:
+        target = np.zeros((array.type.length, len(array.fields)))
+        target[:, ind] = 1
+        # # Define the position of the scores and true labels
+        # test_if_int = np.vectorize(float.is_integer)
+        # labels_mask = {cl_name: test_if_int(events[self.cls_name][cl_name]).all()
+        #             for cl_name in events[self.cls_name].fields}
+        # assert (sum(labels_mask.values()) == 1), (
+        #     f"Only one `true_labels` column per Network is expected, however {sum(labels_mask.values())} were found!"
+        # )
+        return target
+
+    def get_plot_data(self, events: dict):
+        cls_labels = []
+        arrays = ak.Array([])
+        true_labels = np.array([])
+
+        for ind, (label, pred) in enumerate(events.items()):
+            cls_labels.append(label)
+            arrays = ak.concatenate((arrays, pred))
+            true_labels = np.concatenate((true_labels, self.get_target_labels(pred, ind))) if ind else self.get_target_labels(pred, ind)
+
+        pred_labels = [l.lstrip('score_') for l in arrays.fields]
+        arrays = ak.to_numpy(arrays)
+        arrays = arrays.view((arrays.dtype[0], len(arrays.dtype.names)))
+
+        return arrays, true_labels, cls_labels, pred_labels
+
     @law.decorator.log
     @law.decorator.localize
     @law.decorator.safe_output
@@ -61,18 +90,12 @@ class PlotMLMetric(PlotBase):
         output = self.output()
         events = {}
 
-        from IPython import embed; embed()
         for (_,_,label), path in inputs.items():
             _, path = path.popitem()
             array = path[0]['mlcolumns'].load()
-            events[label] = array['test'] #TODO self.ml_model stattdessen verwenden
-
-        exit()
-        # Define the position of the scores and true labels
-        test_if_int = np.vectorize(float.is_integer)
-        labels_mask = {cl_name: test_if_int(events[self.cls_name][cl_name]).all()
-                       for cl_name in events[self.cls_name].fields}
-        assert (sum(labels_mask.values()) == 1), (
-            f"Only one `true_labels` column per Network is expected, however {sum(labels_mask.values())} were found!"
-        )
+            if events.get(label):
+                events[label] = ak.concatenate((events[label], array['test']))
+            else:
+                events[label] = array['test'] #TODO self.ml_model stattdessen verwenden
+        array, target, cls_labels, pred_labels = self.get_plot_data(events)
 
